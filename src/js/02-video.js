66a26033d19c7dd05562717ecb3cd21d5f9f1b91
OlehKhv <hvostik.oleg@ukr.net>
2023-07-16 22:59:08 +0300

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,18 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(saveCurrentPlayTime, 10000));
+player.on('timeupdate', throttle(saveCurrentPlayTime, 1000));
 
 function saveCurrentPlayTime(e) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(e.seconds));
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
     console.log(e.seconds);
 }
 
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? 0);
+player.setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time')) ?? 0
+);
 
 // player.off('timeupdate');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
+
+if (localStorage.getItem('feedback-form-state')) {
+    const { email, message } = JSON.parse(
+        localStorage.getItem('feedback-form-state')
+    );
+
+    inputEl.value = email;
+    textareaEl.value = message;
+}
+
+function onInput(e) {
+    const userData = { email: '', message: '' };
+    if (e.target.nodeName === 'INPUT') {
+        userData.email = e.target.value;
+        userData.message = textareaEl.value;
+    } else {
+        userData.message = e.target.value;
+        userData.email = inputEl.value;
+    }
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    const { email, message } = e.currentTarget.elements;
+
+    if (!email.value || !message.value) {
+        alert('Всі поля повинні бути заповнені!');
+        return;
+    }
+
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+
+    formEl.reset();
+
+    localStorage.clear();
+}
