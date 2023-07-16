0b267d7c228afd635536e1ffdbd6deb7e47261df
OlehKhv <hvostik.oleg@ukr.net>
2023-07-16 18:20:31 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const listEl = document.querySelector('.gallery');
+
+addMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+function addMarkup(arrGallery) {
+    const markup = arrGallery
+        .map(
+            ({ preview, original, description }) =>
+                `<li class="gallery__item">
+                    <a class="gallery__link" href=${original}>
+                        <img
+                            class="gallery__image"
+                            src=${preview}
+                            alt='${description}'
+                        />
+                    </a>
+                </li>`
+        )
+        .join('');
+    listEl.innerHTML = markup;
+}
